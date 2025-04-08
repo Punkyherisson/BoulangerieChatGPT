c1158363267c3c43a19992658648ffe4b842adeb
Punkyherisson <153446211+Punkyherisson@users.noreply.github.com>
2025-04-08 23:12:31 +0200

    feat: ajout du fichier machines et qualifemploye (version 0.17)

---
+++ data/machine_boulangerie.py
@@ -0,0 +1,52 @@
+{
+    "four": {
+        "type": "électrique",
+        "capex": 10000,
+        "opex_mensuel": 120
+    },
+    "congelateur": {
+        "type": "armoire",
+        "capex": 3000,
+        "opex_mensuel": 40
+    },
+    "petrin": {
+        "type": "spiral",
+        "capex": 5000,
+        "opex_mensuel": 50
+    },
+    "chambre_de_pousse": {
+        "type": "controlée",
+        "capex": 4000,
+        "opex_mensuel": 35
+    },
+    "laminoir": {
+        "type": "manuel",
+        "capex": 4500,
+        "opex_mensuel": 30
+    },
+    "batteur_melangeur": {
+        "type": "20L",
+        "capex": 2500,
+        "opex_mensuel": 25
+    },
+    "vitrine_refrigeree": {
+        "type": "droite",
+        "capex": 3000,
+        "opex_mensuel": 30
+    },
+    "machine_a_cafe": {
+        "type": "professionnelle",
+        "capex": 1500,
+        "opex_mensuel": 20
+    },
+    "trancheuse_a_pain": {
+        "type": "automatique",
+        "capex": 1800,
+        "opex_mensuel": 15
+    },
+    "balance_electronique": {
+        "type": "precision",
+        "capex": 200,
+        "opex_mensuel": 5
+    }
+}
---
+++ data/qualifemploye.py
@@ -0,0 +1,34 @@
+QUALIFICATIONS_EMPLOYES = {
+    "boulanger": {
+        "experience_moyenne_mois": 60,  # 5 ans
+        "salaire_moyen": 1800
+    },
+    "patissier": {
+        "experience_moyenne_mois": 72,  # 6 ans
+        "salaire_moyen": 1900
+    },
+    "vendeur": {
+        "experience_moyenne_mois": 24,  # 2 ans
+        "salaire_moyen": 1600
+    },
+    "livreur": {
+        "experience_moyenne_mois": 12,  # 1 an
+        "salaire_moyen": 1550
+    },
+    "apprenti_boulanger": {
+        "experience_moyenne_mois": 6,
+        "salaire_moyen": 800
+    },
+    "chef_boulanger": {
+        "experience_moyenne_mois": 120,
+        "salaire_moyen": 2500
+    },
+    "chef_patissier": {
+        "experience_moyenne_mois": 132,
+        "salaire_moyen": 2700
+    },
+    "vendeur_expert": {
+        "experience_moyenne_mois": 48,
+        "salaire_moyen": 1800
+    }
+}
